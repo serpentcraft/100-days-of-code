efae09b901ccae9ce52027953cafb773af753341
Daria <darspitsyna@gmail.com>
2026-09-16 17:10:18 +0500

    Update and rename day01.py to day_01.py

--- day_01.py
+++ day_01.py
@@ -1,4 +1,4 @@
-#I wanted to make it a bit more interactive and fun, so I used some things that I learned couple of days ago, mostly I wanted to practice using it.
+# I wanted to make it a bit more interactive and fun, so I used some things that I learned couple of days ago, mostly I wanted to practice using it.
   
 from time import sleep
 string = "Welcome to the Band Name Generator!"
@@ -6,16 +6,19 @@ for letter in string:
  print(letter, end="", flush=True)
  sleep(0.2)
 print()
+
 string = "\nYou will be asked two simple questions."
 for letter in string:
  print(letter, end="", flush=True)
  sleep(0.2)
 print()
+
 string = "\nAnd this will be your band name."
 for letter in string:
  print(letter, end="", flush=True)
  sleep(0.2)
 print()
+
 answer = input("Continue? [y/n]: ")
 if answer.lower() == "y":
  city = input("What is the name of your city you grew up in? \n")
