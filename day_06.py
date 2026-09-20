d63075104bc320073f0416f5d77b817b77ae75bd
Daria <darspitsyna@gmail.com>
2026-09-20 19:27:14 +0500

    Add Escaping the Maze

---
+++ day_06.py
@@ -0,0 +1,15 @@
+# Reeborg's World - maze trial https://reeborg.ca/
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
